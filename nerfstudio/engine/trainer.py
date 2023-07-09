6158dd3e9a54ac61dbad0076fd3a14a672a54de6
kywind <m74002@outlook.com>
2023-07-09 00:35:38 -0700

    specify wandb run name

--- nerfstudio/engine/trainer.py
+++ nerfstudio/engine/trainer.py
@@ -182,7 +182,7 @@ class Trainer:
         # set up writers/profilers if enabled
         writer_log_path = self.base_dir / self.config.logging.relative_log_dir
         writer.setup_event_writer(
-            self.config.is_wandb_enabled(), self.config.is_tensorboard_enabled(), log_dir=writer_log_path
+            self.config.is_wandb_enabled(), self.config.is_tensorboard_enabled(), log_dir=writer_log_path, name=self.config.timestamp
         )
         writer.setup_local_writer(
             self.config.logging, max_iter=self.config.max_num_iterations, banner_messages=banner_messages
